113e8c0b9d41c1a948ee1ad26cc6528ac53cca38
Roche, Devin <Devin.Roche@cdk.com>
2017-07-10 13:28:36 -0700

    Get movie titles from actor/actress
    use lodash to parse array

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const unirest = require('unirest');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const _ = require('lodash');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -14,13 +15,17 @@ app.get('/', function(req, res) {
 app.post('/', function(req, res) {
   let searchActor = req.body.actor;
   let url = `https://community-netflix-roulette.p.mashape.com/api.php?actor=${searchActor}`
+  let responseTxt = "";
   unirest.get(url)
     .header("X-Mashape-Key", "Ole1Gv2CajmshmIErnYAtZtaK9iHp1Rkjv1jsnu3RYLMqETD5X")
     .header("Accept", "application/json")
     .end(function (result) {
-      console.log(result.body);
-      let responseTxt = result.body;
-      res.send(responseTxt)
+      let responseTxt = _.map(result.body, 'show_title');
+      _.forEach(responseTxt, function(title){
+        res.write(title);
+        res.write('\n')
+      })
+      res.end()
     });
 })
 app.listen(3000, function() {
