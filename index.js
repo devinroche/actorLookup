f831b8abc3b23929b1f483287cb9f5c76f7cb8d5
Roche, Devin <Devin.Roche@cdk.com>
2017-07-12 10:20:21 -0700

    lit

--- index.js
+++ index.js
@@ -3,21 +3,29 @@ const unirest = require('unirest');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
 const mongoose = require('mongoose');
-const methodOverride = require ('method-override');
+const methodOverride = require('method-override');
 const app = express();
 
 //CONFIGURATION
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json())
-app.use(bodyParser.json({type: 'application.vnd.api+json'}))
+app.use(bodyParser.json({
+  type: 'application.vnd.api+json'
+}))
 app.use(methodOverride())
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-  res.render('index', {showTitle: null, error: null});
-  res.sendfile('index.html')
+  res.render('index', {
+    showTitle: null,
+    rating: null,
+    avg: null,
+    error: null
+  });
 })
 
 app.post('/', function(req, res) {
@@ -27,10 +35,15 @@ app.post('/', function(req, res) {
   unirest.get(url)
     .header("X-Mashape-Key", "Ole1Gv2CajmshmIErnYAtZtaK9iHp1Rkjv1jsnu3RYLMqETD5X")
     .header("Accept", "application/json")
-    .end(function (result) {
-      let responseTxt = _.map(result.body, 'show_title');
-      console.log(responseTxt);
-      res.render('index', {showTitle: responseTxt})
+    .end(function(result) {
+      let resTitles = _.map(result.body, 'show_title');
+      let resRating = _.map(result.body, 'rating');
+      let resRatingAvg = _.mean(resRating);
+      res.render('index', {
+        showTitle: resTitles,
+        rating: resRating,
+        avg: resRatingAvg
+      })
     });
 })
 app.listen(3000, function() {
