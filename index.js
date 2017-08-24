31dd70f30734bd08bb8dc3c3ffa908cc70833b96
Devin Roche <Roche.devin@ymail.com>
2017-08-24 13:45:12 -0700

    fixed http error

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const unirest = require('unirest');
 const bodyParser = require('body-parser');
-const http = require('http');
 const _ = require('lodash');
 const app = express();
 
@@ -21,7 +20,6 @@ app.get('/', function(req, res) {
   res.render('index', {
     showTitle: null,
     rating: null,
-    //showPoster: null,
     avg: null,
     error: null
   });
@@ -31,14 +29,10 @@ app.post('/', function(req, res) {
   let searchActor = req.body.actor;
   let url = `https://community-netflix-roulette.p.mashape.com/api.php?actor=${searchActor}`
   let responseTxt = "";
-  http.get(url, function () {
-    console.log('fart')
-  })
   unirest.get(url)
     .header("X-Mashape-Key", "Ole1Gv2CajmshmIErnYAtZtaK9iHp1Rkjv1jsnu3RYLMqETD5X")
     .header("Accept", "application/json")
     .end(function(result) {
-
       res.render('index', {
         searchFor: searchActor,
         showTitle: pathCtrl.getTitles(result.body),
